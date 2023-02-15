fd5aef8c74206c66daf564b67d5349a2eca41072
Nguyen <jn@Nguyens-MacBook-Pro.local>
2023-02-14 23:38:37 -0800

    final commit

--- Amount.py
+++ Amount.py
@@ -1,24 +1,30 @@
 def amount(A, S):
-    # sort the list A in ascending order
     A.sort()
-    # initialize the result list to store the combinations
     result = []
-    # initialize the combination list to store the current combination
     combination = []
-    
     for i in range(len(A)):
         if i > 0 and A[i] == A[i - 1]:
-            continue 
-        
-    combination.append(A[i])
-
-    target = S - A[i]
-
-    if target == 0: 
-        result.append(list(combination)) #appends the target to the list 
-    elif target > 0: 
-        pass 
-    else: 
-        break 
-    combination.pop()
-    
+            continue
+        combination.append(A[i])
+        target = S - A[i]
+        if target == 0:
+            result.append(list(combination))
+        elif target > 0:
+            for j in range(i + 1, len(A)):
+                if j > i + 1 and A[j] == A[j - 1]:
+                    continue
+                combination.append(A[j])
+                target -= A[j]
+                if target == 0:
+                    result.append(list(combination))
+                elif target > 0:
+                    continue
+                else:
+                    break
+                # remove the last element from the combination if the target sum is not equal to 0
+                combination.pop()
+        else:
+            # remove the last element from the combination if the target sum is not equal to 0
+            combination.pop()
+            continue
+    return result
