340a42a0e8114d99d1bee6157e1a129ddf86f23b
Nguyen <jn@Nguyens-MacBook-Pro.local>
2023-02-14 21:38:00 -0800

    First commit wtih intialization of vars

---
+++ Amount.py
@@ -0,0 +1,8 @@
+def amount(A, S):
+    # sort the list A in ascending order
+    A.sort()
+    # initialize the result list to store the combinations
+    result = []
+    # initialize the combination list to store the current combination
+    combination = []
+  
